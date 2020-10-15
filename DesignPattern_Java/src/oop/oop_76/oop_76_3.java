2889294aa859c91d552b5ab28aaa90b3044897c4
jcj <amusement1234@yeah.net>
2020-10-15 15:25:59 +0800

    commit

---
+++ DesignPattern_Java/src/oop/oop_76/oop_76_3.java
@@ -0,0 +1,130 @@
+// 装饰器模式在 Collections 类中的应用
+
+// Collections 类是一个集合容器的工具类，提供了很多静态方法，用来创建各种集合容器，比如通过 unmodifiableColletion() 静态方法，来创建 UnmodifiableCollection 类对象。而这些容器类中的 UnmodifiableCollection 类、CheckedCollection 和 SynchronizedCollection 类，就是针对 Collection 类的装饰器类。
+
+// 因为刚刚提到的这三个装饰器类，在代码结构上几乎一样，所以，我们这里只拿其中的 UnmodifiableCollection 类来举例讲解一下。UnmodifiableCollection 类是 Collections 类的一个内部类，相关代码我摘抄到了下面，你可以先看下。
+
+public class Collections {
+    private Collections() {
+    }
+
+    public static <T> Collection<T> unmodifiableCollection(Collection<? extends T> c) {
+        return new UnmodifiableCollection<>(c);
+    }
+
+    static class UnmodifiableCollection<E> implements Collection<E>, Serializable {
+        private static final long serialVersionUID = 1820017752578914078L;
+        final Collection<? extends E> c;
+
+        UnmodifiableCollection(Collection<? extends E> c) {
+            if (c == null)
+                throw new NullPointerException();
+            this.c = c;
+        }
+
+        public int size() {
+            return c.size();
+        }
+
+        public boolean isEmpty() {
+            return c.isEmpty();
+        }
+
+        public boolean contains(Object o) {
+            return c.contains(o);
+        }
+
+        public Object[] toArray() {
+            return c.toArray();
+        }
+
+        public <T> T[] toArray(T[] a) {
+            return c.toArray(a);
+        }
+
+        public String toString() {
+            return c.toString();
+        }
+
+        public Iterator<E> iterator() {
+            return new Iterator<E>() {
+                private final Iterator<? extends E> i = c.iterator();
+
+                public boolean hasNext() {
+                    return i.hasNext();
+                }
+
+                public E next() {
+                    return i.next();
+                }
+
+                public void remove() {
+                    throw new UnsupportedOperationException();
+                }
+
+                @Override
+                public void forEachRemaining(Consumer<? super E> action) {
+                    // Use backing collection version
+                    i.forEachRemaining(action);
+                }
+            };
+        }
+
+        public boolean add(E e) {
+            throw new UnsupportedOperationException();
+        }
+
+      public boolean remove(Object o) {
+         hrow new UnsupportedOperationException();
+      }
+
+        public boolean containsAll(Collection<?> coll) {
+            return c.containsAll(coll);
+        }
+
+        public boolean addAll(Collection<? extends E> coll) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean removeAll(Collection<?> coll) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean retainAll(Collection<?> coll) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+
+        // Override default methods in Collection
+        @Override
+        public void forEach(Consumer<? super E> action) {
+            c.forEach(action);
+        }
+
+        @Override
+        public boolean removeIf(Predicate<? super E> filter) {
+            throw new UnsupportedOperationException();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Spliterator<E> spliterator() {
+            return (Spliterator<E>) c.spliterator();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Stream<E> stream() {
+            return (Stream<E>) c.stream();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Stream<E> parallelStream() {
+            return (Stream<E>) c.parallelStream();
+        }
+    }
+}
